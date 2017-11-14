875fe46fb12b749163dc586cd65475d682fb6104
Johan Sundgren <johan.sundgren@hotmail.com>
2017-11-14 22:21:45 +0100

    another soundviz

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Soundviz5
+### Sound visualisation made with JS.
+Playing around with the p5.js library pt5. Fun stuff! :-)
+Link to demo [here](https://jsundgren.github.io/Soundviz5/)
---
+++ js/audio.js
@@ -0,0 +1,58 @@
+var song, analyzer;
+var w;
+var filter;
+
+function preload(){
+    song = loadSound('yesterye.mp3');
+}
+
+function setup(){
+    createCanvas(0.75*windowWidth, 0.75*windowHeight,WEBGL);
+    song.loop();
+    fft = new p5.FFT(0.5,128);
+    fft.setInput(song);
+    analyzer = new p5.Amplitude();
+    analyzer.setInput(song);
+    w = width / 128;
+}
+
+function draw(){
+    background(173,226,255);
+    var spectrum = fft.analyze();
+    var rms = analyzer.getLevel();
+    translate(0,0,-1600);
+    rotateX(-frameCount*0.001);
+    scale(0.3,0.3,0.3);
+    push();
+        noFill();
+        rotateY(-frameCount*0.01);
+        sphere(3000+rms*800);
+    pop();
+    rotateY(frameCount*0.015);
+    push();
+        for(var i = 0; i < spectrum.length; i++){
+            var amp = spectrum[i];
+            var y = map(amp, 0, 255, 0, 11.5*height);
+            fill(255);
+            stroke(0);
+            push();
+                translate(2.6*i*w,0,0);
+                box(w, y, 60);
+            pop();
+        }
+    pop();
+}
+
+function pause(){
+    if(song.isPlaying()){
+        song.pause();
+        document.getElementById("ppToggle").innerHTML = "&#9654;";
+    }else{
+        song.play();
+        document.getElementById("ppToggle").innerHTML = "&#9724;";
+    }
+}
+
+function windowResized() {
+  resizeCanvas(0.75*windowWidth, 0.75*windowHeight);
+}
